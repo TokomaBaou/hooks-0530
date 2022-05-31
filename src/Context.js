e544036f37c22c6a58ad69cf5e18677b6f0596c7
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-31 20:11:56 +0900

    Context

---
+++ src/Context.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const Context = createContext({});
+
+export const Provider = (props) => {
+  const { children } = props;
+  const obj = { value: test };
+
+  return <Context.Provider value={obj}>{children}</Context.Provider>;
+};
