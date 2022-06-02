25cc9a303999076a898a643951f83965b0ba42f9
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-06-02 23:05:14 +0900

    useState textの保持

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const Input = () => {
+  /**入力された値を保持するstate */
+  const [text, setText] = useState("");
+
+  /**inputの入力チェンジを変数に入れる、その際にステートをセットする */
+  /**e.target.valueで値を取得する */
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <input value="text" placeholder="ここに入力してください"></input>
+      <input
+        value={text}
+        onChange={onChange}
+        placeholder="ここに入力してください"
+      ></input>
+      <p>{text}</p>
     </>
   );
 };
